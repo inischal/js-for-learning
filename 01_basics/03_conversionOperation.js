0bf569d22b3918e2857b1176fcc88796fe9aab88
Nischal Shrestha <nspnischal@gmail.com>
2023-11-27 07:42:59 +0000

    converson of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33";
+// console.log(score);
+
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+//conversion
+//"33"==>33
+//"33qwe"=>NaN ,Not a Number
+//Null=>0
+//true=1
+//false=0
+
+
+let loggedIn=1;
+let booleanLoggedIn=Boolean(loggedIn);
+let booleanToNumber=Number(booleanLoggedIn);
+console.log(booleanLoggedIn,booleanToNumber);
