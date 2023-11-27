2163321a917f1bddd6fdf37f99bf49580ceb9be9
Nischal Shrestha <nspnischal@gmail.com>
2023-11-27 06:52:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert(4+3) //we are using nodejs, not browser
+
+//primitives data types:
+let name="nischal"; //string
+let age=18; //number
+// isLoggedIn=true; //boolean
+//bigint
+//null, is a stand alone value.
+//undefined, considered as value has not been assigned.
+//symbol, used for uniquness.
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
