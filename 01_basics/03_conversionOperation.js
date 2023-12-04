c0b33547058527174b5eed27c69532f2b9c874c8
Nischal Shrestha <nspnischal@gmail.com>
2023-12-04 16:35:50 +0000

    more info in conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,4 +17,22 @@ let valueInNumber=Number(score);
 let loggedIn=1;
 let booleanLoggedIn=Boolean(loggedIn);
 let booleanToNumber=Number(booleanLoggedIn);
-console.log(booleanLoggedIn,booleanToNumber);
+// console.log(booleanLoggedIn,booleanToNumber);
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+let str1="hello ";
+let str2="Nischal";
+let str3=str1+str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("1" + 2 + 2);
+// console.log(1 + 1 + 2 + "2");
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
