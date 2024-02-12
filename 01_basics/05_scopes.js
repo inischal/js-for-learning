581bf79d5f4a84f7ca5a7d8431e30e9d906d538f
Nischal Shrestha <nspnischal@gmail.com>
2024-02-12 13:45:53 +0000

    understanding of scopes

---
+++ 01_basics/05_scopes.js
@@ -0,0 +1,11 @@
+let a=340;
+
+if(true){
+    let a=10;
+    console.log("INNER a:",a);
+    // const b= 20;
+    // var c= 30;
+}
+// console.log(c);
+// console.log(b);
+console.log("outer a:",a);
