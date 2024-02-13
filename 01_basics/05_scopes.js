f30ee5e1bce0220360c72babc32422d186714769
Nischal Shrestha <nspnischal@gmail.com>
2024-02-13 11:08:05 +0000

    more learning about scope and something about hoisting

--- 01_basics/05_scopes.js
+++ 01_basics/05_scopes.js
@@ -1,11 +1,48 @@
-let a=340;
+// let a=340;
 
-if(true){
-    let a=10;
-    console.log("INNER a:",a);
+// if(true){
+//     let a=10;
+//     console.log("INNER a:",a);
     // const b= 20;
     // var c= 30;
-}
+// }
 // console.log(c);
 // console.log(b);
-console.log("outer a:",a);
+// console.log("outer a:",a);
+
+// function one(){
+//     const username="Nischal";
+//     function two(){
+//         const website="Youtube";
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     // two();
+
+// }
+// one();
+
+if(true){
+    const username="Nischal";
+    if(username==="Nischal"){
+        const website=" Youtube";
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+//++++++ intersting +++++ some glimpse of hoisting
+addone(2);
+function addone(num){
+    return num+1;
+}
+
+const addfive=function(num){
+    return num+5;
+}
+addfive(6);
+
+
